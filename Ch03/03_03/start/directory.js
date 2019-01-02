0da5d224689f0f3aa46127dd159e1e7dc7339715
Joe Chellman <joe@shooflydesign.org>
2019-01-01 20:10:57 -0800

    Ch03 updates

--- Ch03/03_03/start/directory.js
+++ Ch03/03_03/start/directory.js
@@ -1,16 +1,16 @@
 (function() {
   "use strict";
 
-  Vue.component('directory-filters', {
+  Vue.component("directory-filters", {
     template: '<form action="" id="directory-filters"><p>Insert filters here</p></form>'
   });
 
-  Vue.component('directory-people', {
-    template: '<p>Insert people here</p>'
+  Vue.component("directory-people", {
+    template: "<p>Insert people here</p>"
   });
 
   var Directory = new Vue({
-    el: '#directory-root',
+    el: "#directory-root",
     data: {
       people: window.LMDirectory.people
     }
